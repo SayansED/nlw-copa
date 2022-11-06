9692f18ffbf57f629d4c35a4f641a902d2c123b0
Eduardo <edusayans@gmail.com>
2022-11-06 12:51:26 -0300

    Atualizando

--- main.js
+++ main.js
@@ -8,29 +8,23 @@ function createGame(player1, hour, player2) {
   `
 }
 
+let delay = 3;
 function createCard(date, day, games) {
+  delay = delay + 0.3;
   return `
-  <div class="card">
-                <h2>${date} <span>${day}</span></h2>
-                <ul>
-                   ${games}
-                </ul>
-            </div>
+  <div class="card" style="animation-delay: ${delay}s}">
+    <h2>${date} <span>${day}</span></h2>
+    <ul>${games}</ul>
+  </div>
   `
 }
 
-document.querySelector("#app").innerHTML = `
-  <header>
-    <img src="./assets/logo.svg" alt="logo">
-  </header>
-  <main id="cards">
-    ${createCard(
-      "24/11",
-      "Quinta",
-      createGame("switzerland", "07:00", "cameroon") +
-      createGame("uruguay", "10:00", "south korea") +
-      createGame("portugal", "13:00", "ghana") +
-      createGame("brazil", "16:00", "serbia")
-    )}
-  </main>
-`
+document.querySelector("#cards").innerHTML = 
+  createCard("24/11","Quinta", 
+  createGame("switzerland", "07:00", "cameroon") + createGame("uruguay", "10:00", "south korea") +
+  createGame("portugal", "13:00", "ghana") +
+  createGame("brazil", "16:00", "serbia")
+  ) +
+  createCard("24/11","Quinta", 
+  createGame("switzerland", "07:00", "cameroon"))
+
